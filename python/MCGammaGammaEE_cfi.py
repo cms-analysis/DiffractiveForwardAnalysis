c0aac6ec6d42c4f7eab8b90e3b9e3c3d67ca2b6d
cmsbuild <>
2008-08-27 07:24:40 +0000

    This commit was manufactured by cvs2git to create tag 'DiffractiveForwardAnalysis-GammaGammaLeptonLepton-V00-02-03'.
    Sprout from master 2008-08-27 07:24:39 UTC Jonathan Jason Hollar <sha1-3bc98e2026a4429734ea27d94d135c21bfcfff70@cern.ch> 'Updates for CMSSW_2_0_X:'
    Delete:
        python/GammaGammaEE_cfi.py
        python/GammaGammaMuMu_cfi.py
        python/HLTFilter_cfi.py
        python/MCGammaGammaEE_cfi.py
        python/MCGammaGammaMuMu_cfi.py
        python/PATGammaGammaEE_cfi.py
        python/PATGammaGammaMuMu_cfi.py
        python/__init__.py

--- python/MCGammaGammaEE_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-mcgamgameeanalysis = cms.EDFilter("GammaGammaEEMC",
-    outfilename = cms.untracked.string('ee.genlevel.root'),
-    FillAllMCParticles = cms.bool(False)
-)
-
-
-
