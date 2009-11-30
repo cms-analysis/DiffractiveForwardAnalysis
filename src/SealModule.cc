4e1cd8a11b7db7dd2471f45bdf982037a506200b
Jonathan Jason Hollar <sha1-3bc98e2026a4429734ea27d94d135c21bfcfff70@cern.ch>
2009-11-30 13:28:48 +0000

    900 GeV Collisions analysis code from Nicolas

--- src/SealModule.cc
+++ src/SealModule.cc
@@ -1,18 +1,10 @@
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 
-#include "DiffractiveForwardAnalysis/GammaGammaLeptonLepton/interface/GammaGammaMuMu.h"
-#include "DiffractiveForwardAnalysis/GammaGammaLeptonLepton/interface/GammaGammaEE.h"
-#include "DiffractiveForwardAnalysis/GammaGammaLeptonLepton/interface/GammaGammaMuMuMC.h"
-#include "DiffractiveForwardAnalysis/GammaGammaLeptonLepton/interface/GammaGammaEEMC.h"
 #include "DiffractiveForwardAnalysis/GammaGammaLeptonLepton/interface/CosmicsMuMu.h"
-#include "DiffractiveForwardAnalysis/GammaGammaLeptonLepton/interface/SingleBeamMuMu.h"
+#include "DiffractiveForwardAnalysis/GammaGammaLeptonLepton/interface/CollisionsMuMu.h"
 
 DEFINE_SEAL_MODULE();
-DEFINE_FWK_MODULE(GammaGammaMuMu);
-DEFINE_ANOTHER_FWK_MODULE(GammaGammaEE);
-DEFINE_FWK_MODULE(GammaGammaMuMuMC);
-DEFINE_ANOTHER_FWK_MODULE(GammaGammaEEMC);
-DEFINE_ANOTHER_FWK_MODULE(CosmicsMuMu);
-DEFINE_ANOTHER_FWK_MODULE(SingleBeamMuMu);
+DEFINE_FWK_MODULE(CosmicsMuMu);
+DEFINE_ANOTHER_FWK_MODULE(CollisionsMuMu);
 
