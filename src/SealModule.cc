8e242bbb3f87277bdafb2963a8e6958bfa16ee35
Laurent <laurent.forthomme@cern.ch>
2014-03-03 10:20:16 +0100

    Removed the SealModule as it is not required anymore

--- src/SealModule.cc
+++
@@ -1,7 +0,0 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "DiffractiveForwardAnalysis/GammaGammaLeptonLepton/interface/GammaGammaLL.h"
-
-//DEFINE_SEAL_MODULE();
-DEFINE_FWK_MODULE(GammaGammaLL);
